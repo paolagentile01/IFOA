b3c23be5d374888684f099955d9f13e84976475c
paolagentile01 <paolagentile01@libero.it>
2023-11-27 18:23:28 +0100

    commit

---
+++ sett5/progetto_settimanale_medium.com/assets/js/script.js
@@ -0,0 +1,11 @@
+let navbar = document.querySelector(".nav-bar-tag");
+let button = document.querySelector(".button-link a");
+  window.addEventListener("scroll", function() {
+    if (window.scrollY > 300) {
+        navbar.classList.add("topMenu");
+        button.classList.add("topButton");
+    } else {
+        navbar.classList.remove("topMenu"); 
+        button.classList.remove("topButton"); 
+    }
+});
