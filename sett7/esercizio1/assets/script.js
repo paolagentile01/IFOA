326f3057a48eeb0e9a6ed1cd409b0c2bb2419a4b
paolagentile01 <paolagentile01@libero.it>
2023-12-05 14:38:03 +0100

    commit

--- sett7/esercizio1/assets/script.js
+++ sett7/esercizio1/assets/script.js
@@ -1,10 +1,2 @@
 /* ESERCIZIO 1*/
-
-function concatena(stringa1, stringa2){
-let first = stringa1.slice(0, 2);
-let second = stringa2.slice(stringa2.length - 3);
-let third = first.concat(second);
-return third.toUpperCase();
-}
-
-console.log(concatena("Ciao", "Mondo"));
+console.log("ciao");
