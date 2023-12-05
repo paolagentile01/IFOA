1f30c8823e8608f5952c5fb2338695d100a0c0d1
paolagentile01 <paolagentile01@libero.it>
2023-12-05 14:28:49 +0100

    commit

--- sett7/esercizio1/assets/script.js
+++ sett7/esercizio1/assets/script.js
@@ -1,7 +1,4 @@
-/* ESERCIZIO 1
-  Scrivi una funzione per concatenare due stringhe ricevute come parametri, selezionando solamente i primi 2 caratteri della
-  prima e gli ultimi 3 della seconda. Converti la stringa risultante in maiuscolo e mostrala con un console.log().
-*/
+/* ESERCIZIO 1*/
 
 function concatena(stringa1, stringa2){
 let first = stringa1.slice(0, 2);
