42d1de1e0e8d341a8259b01b2f63d0c197274c94
paolagentile01 <paolagentile01@libero.it>
2023-12-05 16:27:01 +0100

    commit

--- sett7/esercizio1/assets/script.js
+++ sett7/esercizio1/assets/script.js
@@ -1,2 +1,23 @@
 /* ESERCIZIO 1*/
-console.log("ciao");
+class User {
+    constructor(firstName, lastName, age, location){
+       this.firstName = firstName;
+       this.lastName = lastName;
+       this.age = age;
+       this.location = location;
+    }
+
+    getConfrontoAge(x, y){
+    if(x.age > y.age){
+    console.log(x.firstName + ' è più vecchio di '+ y.firstName);
+    } else if(x.age < y.age){
+        console.log(y.firstName + ' è più vecchio di '+ x.firstName);
+    } else if (x.age === y.age){
+        console.log(x.firstName +' e '+ y.firstName + '  hanno la stessa età');
+    }
+}
+}
+
+const user1 = new User("Mario", "Rossi", 25, "Milano");
+const user2 = new User("Giacomo", "Verdi", 20, "Roma");
+user1.getConfrontoAge(user1, user2);
