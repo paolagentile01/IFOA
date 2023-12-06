90df704549f291dcf8886b1fa0f7c9519c889389
paolagentile01 <paolagentile01@libero.it>
2023-12-06 16:50:09 +0100

    commit

--- sett7/esercizio2/assets/script.js
+++ sett7/esercizio2/assets/script.js
@@ -1,6 +1,23 @@
 /* ESERCIZIO 1*/
+let userName = document.getElementById("user-name")
 let saveButton = document.getElementById("bottone-save");
 let removeButton = document.getElementById("bottone-remove");
+let savedValue = document.getElementById("saved-value");
+
+saveButton.onclick  = function(){
+    localStorage.setItem("user", userName.value);
+    showValue ();
+}
+
+function showValue(){
+    savedValue.innerText = userName.value;
+}
+
+
+removeButton.onclick = function(){
+    localStorage.removeItem("user");
+    savedValue.innerText = "";
+}
 /* ESERCIZIO 2*/
 
 
